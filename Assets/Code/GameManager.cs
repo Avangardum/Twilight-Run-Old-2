10a693268222cb0dabe0fadc6bd32f7004b20a43
Avangardum <yurykuz65@gmail.com>
2022-05-25 10:33:52 +0300

    Made tracks segments generation

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -5,11 +5,18 @@ namespace Avangardum.TwilightRun
 {
     class GameManager : MonoBehaviour, IGameManager
     {
+        private const float LevelGenerationRange = 20;
+        private const float LevelCleanupRange = 5;
+        
         private ILevelGenerator _standardLevelGenerator;
         private ILevelGenerator _tutorialLevelGenerator;
+        private ILevelGenerator _currentLevelGenerator;
         private IPlayerCharactersController _playerCharactersController;
         private IInputManager _inputManager;
+        private bool _isGameActive;
 
+        [SerializeField] private GameObject _whitePlayerCharacter;
+        
         public void InjectDependencies(ILevelGenerator standardLevelGenerator, ILevelGenerator tutorialLevelGenerator, 
             IPlayerCharactersController playerCharactersController, IInputManager inputManager)
         {
@@ -19,6 +26,12 @@ namespace Avangardum.TwilightRun
             _inputManager = inputManager;
 
             inputManager.Tap += OnTap;
+            playerCharactersController.CharacterDied += OnCharacterDied;
+        }
+
+        private void OnCharacterDied(object sender, EventArgs e)
+        {
+            _isGameActive = false;
         }
 
         private void OnTap(object sender, EventArgs e)
@@ -29,11 +42,20 @@ namespace Avangardum.TwilightRun
         public void StartGame()
         {
             _playerCharactersController.Reset();
+            _currentLevelGenerator = _standardLevelGenerator;
+            _currentLevelGenerator.ClearAll();
+            _isGameActive = true;
         }
 
         public void StartTutorial()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
+        }
+
+        private void Update()
+        {
+            _currentLevelGenerator.GenerateToThePoint(_whitePlayerCharacter.transform.position.z + LevelGenerationRange);
+            _currentLevelGenerator.ClearBehindThePoint(_whitePlayerCharacter.transform.position.z - LevelCleanupRange);
         }
     }
 }
--- Assets/Code/StandardLevelGenerator.cs
+++ Assets/Code/StandardLevelGenerator.cs
@@ -1,22 +1,63 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Avangardum.TwilightRun
 {
     class StandardLevelGenerator : MonoBehaviour, ILevelGenerator
     {
+        [SerializeField] private GameObject _tracksSegmentPrefab;
+        [SerializeField] private float _tracksSegmentLength;
+        
+        private List<GameObject> _levelElements = new List<GameObject>();
+        private float _generationFrontZ;
+        private float _trackFrontZ;
+        private float _elementsFrontZ;
+        private bool _isZeroTrackSegmentGenerated;
+        
         public void GenerateToThePoint(float point)
         {
-            throw new System.NotImplementedException();
+            // generate zero track segment if not exists
+            if (!_isZeroTrackSegmentGenerated)
+            {
+                var zeroTrackSegment = Instantiate(_tracksSegmentPrefab, new Vector3(0, 0, -_tracksSegmentLength), Quaternion.identity);
+                _levelElements.Add(zeroTrackSegment);
+            }
+            
+            // generate tracks segments
+            for (; _trackFrontZ < point; _trackFrontZ += _tracksSegmentLength)
+            {
+                var trackSegment = Instantiate(_tracksSegmentPrefab, new Vector3(0, 0, _trackFrontZ), Quaternion.identity);
+                _levelElements.Add(trackSegment);
+            }
+            
+            // generate elements
         }
 
         public void ClearBehindThePoint(float point)
         {
-            throw new System.NotImplementedException();
+            foreach (var element in _levelElements.ToList())
+            {
+                float frontZ = element.CompareTag("TrackSegment") ? element.transform.position.z + _tracksSegmentLength : element.transform.position.z;
+                if (frontZ < point)
+                {
+                    Destroy(element);
+                    _levelElements.Remove(element);
+                }
+            }
         }
 
         public void ClearAll()
         {
-            throw new System.NotImplementedException();
+            foreach (var element in _levelElements)
+            {
+                Destroy(element);
+            }
+            _levelElements.Clear();
+            _generationFrontZ = 0;
+            _trackFrontZ = 0;
+            _elementsFrontZ = 0;
+            _isZeroTrackSegmentGenerated = false;
         }
     }
 }
